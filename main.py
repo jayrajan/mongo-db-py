513d3a69e9d04077ab2d9cbc34046e9bc41890ba
Jerin Rajan <jrajan@jprtech.co.uk>
2022-05-02 17:50:44 +0100

    feat: created users db and collection, putting in sample data

--- main.py
+++ main.py
@@ -5,15 +5,35 @@ import config.DbConfig as config
 # Connect to mongodb
 
 try:
-    db = config.DbConfig()
-    client = pym.MongoClient(db.url)
-    print(client.server_info())
+    dbConfig = config.DbConfig()
+    client = pym.MongoClient(dbConfig.url)
+    print("Conenction to MongoDb Established")
+    # print(client.server_info())
 except pym.errors.ServerSelectionTimeoutError as err:
     print(err)
 
+# create a database called Users
+db = client.users
 
-# schema
+# data
+firstName = ["jay","ed","abi","sherin","aaron","steven"]
+secondName = ["r","w","w","a","a","l"]
+age = [22,33,22,14,6,3]
+
+for i in range(len(firstName)):
+    users = {
+        "firstName" : firstName[i],
+        "secondName" : secondName[i],
+        "age" : age[i]
+    }
+    # insert users into a collection called 
+    # Putting users data
+    result = db.users.insert_one(users)
+
+print("User data added to the collection")
+# CRUD operations
 
 
 # Disconnect from mongodb
 client.close()
+print("Connection to mongoDb Disconnected")
