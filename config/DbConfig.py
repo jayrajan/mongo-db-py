95cfa1c84cebaf27cc69a17dfeaaa455e3fbf4ef
Jerin Rajan <jrajan@jprtech.co.uk>
2022-05-02 14:07:07 +0100

    fix: minor

--- config/DbConfig.py
+++ config/DbConfig.py
@@ -1,8 +1,9 @@
+import pymongo
+
 class DbConfig:
     userName = "user-01"
     passWord = "dm8ONKwtiZQA7Vpc"
     url = "mongodb+srv://{username}:{password}@cluster0.laopn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(username=userName,password=passWord)
 
-    # url = "mongodb+srv://{username}:{password}@cluster0.laopn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(username=userName,password=passWord)
-    # url = "mongodb+srv://cluster0.laopn.mongodb.net/myFirstDatabase?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+
 
